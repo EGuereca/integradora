450556d156a248f8b46267d27d52774a3a210f30
GeraOR <146026356+GeraOR@users.noreply.github.com>
2024-07-15 21:30:18 -0600

    Productos
    Vista base de productos

---
+++ LaSombraV2/LA SOMBRA/HTML/productos.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productos - La Sombra</title>
+    <link rel="stylesheet" href="../bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../CSS/productos.css">
+</head>
+<body>
+    <header id="inicio" class="row header">
+        <div class="user-cart col-lg-4 col-sm-6">
+            <a href="../HTML/inicio-sesion.html"><img src="../ICONS/user.png" alt="user"></a>
+            <a href="../index.html"><img src="../ICONS/cart.png" alt="cart"></a>
+        </div>
+        <div class="logo col-lg-4 col-sm-6"><a href="#in"><img src="../IMG/sombra-logo.jpg" alt="La Sombra"></a></div>
+        <nav class="nav col-lg-4 col-sm-12">
+            <a style="color: green;" href="#in">Productos</a>
+            <a href="../HTML/perforaciones.html">Perforaciones</a>
+            <a href="#suc">Sucursales</a>
+        </nav>
+    </header>
+    <div class="container" id="#in">
+        <div class="search-bar">
+            <input type="text" class="form-control" placeholder="Buscar artículo...">
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="product">
+                    <img src="../IMG/blazy-susann.jpg" alt="Product Image">
+                    <h5>Product</h5>
+                    <p>Description of first product</p>
+                    <p>$10.99</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="product">
+                    <img src="../IMG/blunt-wrapp.jpg" alt="Product Image">
+                    <h5>Product</h5>
+                    <p>Description of second product</p>
+                    <p>$10.99</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="product">
+                    <img src="../IMG/cookiess.jpg" alt="Product Image">
+                    <h5>Product</h5>
+                    <p>Description of third product</p>
+                    <p>$10.99</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="product">
+                    <img src="../IMG/g-rollzzz.jpg" alt="Product Image">
+                    <h5>Product</h5>
+                    <p>Description of fourth product</p>
+                    <p>$10.99</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="product">
+                    <img src="../IMG/lion-rolling-circuss.jpg" alt="Product Image">
+                    <h5>Product</h5>
+                    <p>Description of fifth product</p>
+                    <p>$10.99</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="product">
+                    <img src="../IMG/king-palmm.jpg" alt="Product Image">
+                    <h5>Product</h5>
+                    <p>Description of sixth product</p>
+                    <p>$10.99</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
