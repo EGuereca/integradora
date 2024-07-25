5acdfd3ad97b3a19d5910a2fe90309559b70e4b2
jtalaveraa <jiramtalavera@hotmail.com>
2024-07-25 01:14:14 -0600

    agregué los sign up

---
+++ LaSombraV2/LA SOMBRA/SCRIPTS/crear-cliente.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+try {
+    $conexion = new PDO("mysql:host=localhost;dbname=la_sombra", "root", "Mysql123");
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+
+$stmt = $conexion->prepare("CALL REGISTRO_CLIENTES(?,?,?,?)");
+
+if (isset($_POST["btncrearclient"])) {
+    if (!empty($_POST["usuario"]) && !empty($_POST["password"]) && !empty($_POST["email"]) && !empty($_POST["pass"]) && !empty($_POST["telefono"])) {
+        $pass1 = $_POST['password'];
+        $pass2 = $_POST['pass'];
+
+        if ($pass1 === $pass2) {
+            $usuario = $_POST["usuario"];
+            $password = $_POST["password"];
+            $email = $_POST["email"];
+            $telefono = $_POST["telefono"];
+
+            $stmt->bindParam(1, $usuario, PDO::PARAM_STR);
+            $stmt->bindParam(2, $password, PDO::PARAM_STR);
+            $stmt->bindParam(3, $email, PDO::PARAM_STR);
+            $stmt->bindParam(4, $telefono, PDO::PARAM_STR);
+            $stmt->execute();
+            
+            echo "<p style='color: green;'>Usuario registrado exitosamente.</p>";
+        } else {
+            echo "<p style='color: red;'>Las contraseñas son diferentes.</p>";
+        }
+    } else {
+        echo "Ingrese todos los datos";
+    }
+}
+
+?>
--- LaSombraV2/LA SOMBRA/VIEWS/crear-cuenta.php
+++ LaSombraV2/LA SOMBRA/VIEWS/crear-cuenta.php
@@ -10,7 +10,7 @@
     <div class="container">
         <img src="../IMG/sombra-logo.jpg" alt="La Sombra">
         <h2>Crea tu cuenta</h2>
-        <form>
+        <form  method="post" action="">
             <div class="form-group">
                 <label for="username">Ingresa tu nombre de usuario:</label><br>
                 <input type="text" id="username" placeholder="Ingresa tu nombre de usuario aquí">
@@ -19,6 +19,12 @@
                 <label for="email">Ingresa tu correo:</label><br>
                 <input type="email" id="email" placeholder="ejemplo@mail.com">
             </div>
+
+            <div class="form-group">
+                <label for="telefono">Teléfono:</label><br>
+                <input type="text" id="telefono" placeholder="Ingresa tu telefono" name="telefono" required>
+            </div>
+
             <div class="form-group">
                 <label for="password">Ingresa tu contraseña:</label><br>
                 <input type="password" id="password" placeholder="Ingresa tu contraseña aquí">
@@ -27,11 +33,13 @@
                 <label for="confirm-password">Confirmar contraseña:</label><br>
                 <input type="password" id="confirm-password" placeholder="Ingresa tu contraseña aquí">
             </div>
-            <button type="submit" class="btn btn-green">Crear cuenta</button>
-            O
+            <button type="submit" class="btn btn-green"  name="btncrearclient">Crear cuenta</button>
+            
             <button type="button" onclick="irAOtraPagina()" class="btn btn-grey">Inicia sesión</button>
         </form>
-        
+        <?php
+            include '../SCRIPTS/crear-cliente.php';
+        ?>
         <p class="p-back"><a href="../VIEWS/iniciov2.php" class="back">Regresar a inicio</a></p>
     </div>
     <script>
