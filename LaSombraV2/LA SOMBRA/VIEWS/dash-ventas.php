595f7fb13ca636d84c94047c21c6f8529f29e028
Gera <go47455@gmail.com>
2024-07-31 21:17:24 -0600

    dash básico de ventas

---
+++ LaSombraV2/LA SOMBRA/VIEWS/dash-ventas.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ventas</title>
+    <link rel="stylesheet" href="../bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../CSS/dash-ventas.css">
+</head>
+<body>
+<div class="d-flex">
+        <div class="sidebar">
+            <img src="../IMG/sombra-logo.jpg" alt="La Sombra Logo" class="img-fluid mb-4">
+            <a style="background-color: limegreen;" href="#">Ventas</a>
+            <a href="#">Apartados</a>
+            <a href="#">Productos</a>
+            <a href="#">Citas</a>
+            <a href="#">Proveedor</a>
+            <a href="#">Registrar empleado</a>
+        </div>
+        <div class="content flex-grow-1">
+            <div class="header">
+                <div>
+                    <a href="../VIEWS/dash-nueva-venta.php">Nueva venta</a>
+                    <span>dd/mm/aa</span>
+                    <span>Sucursal</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card card-custom">
+                        <div class="card-header">
+                            Ventas
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>ID Venta</th>
+                                        <th>Total</th>
+                                        <th>ID Empleado</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>00001</td>
+                                        <td>$100.80</td>
+                                        <td>Juan3</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00002</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00003</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00004</td>
+                                        <td>$100.80</td>
+                                        <td>Pepe4</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card-custom">
+                        <div class="card-header">
+                            Monto total del día
+                        </div>
+                        <div class="card-body">
+                            <h3>$403.20</h3>
+                        </div>
+                    </div>
+                    <div class="card card-custom">
+                        <div class="card-header">
+                            00001
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Productos</th>
+                                        <th>Cantidad</th>
+                                        <th>Subtotal</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>00001</td>
+                                        <td>2</td>
+                                        <td>$40.36</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00011</td>
+                                        <td>1</td>
+                                        <td>$10.40</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00101</td>
+                                        <td>1</td>
+                                        <td>$49.36</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00101</td>
+                                        <td>1</td>
+                                        <td>$49.36</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00101</td>
+                                        <td>1</td>
+                                        <td>$49.36</td>
+                                    </tr>
+                                    <tr>
+                                        <td>00101</td>
+                                        <td>1</td>
+                                        <td>$49.36</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
