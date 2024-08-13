6a57454c09ddbffb648d6af54ca4f23fc00b1c2c
aarale <vbaralezarate@gmail.com>
2024-08-13 15:27:10 -0600

    ya se limitan los caracteres y sale la advertencia si ya se esta usando ;)

--- LaSombraV2/LA SOMBRA/VIEWS/crear-cuenta.php
+++ LaSombraV2/LA SOMBRA/VIEWS/crear-cuenta.php
@@ -12,27 +12,32 @@
         <img src="../IMG/sombra-logo.jpg" alt="La Sombra">
         <h2>Crea tu cuenta</h2>
         <form method="post" action="">
-            <div class="form-group">
-                <label for="username">Ingresa tu nombre de usuario:</label><br>
-                <input type="text" id="username" placeholder="Ingresa tu nombre de usuario aquí" name="usuario">
-            </div>
+        <div class="form-group">
+        <label for="username">Ingresa tu nombre de usuario:</label><br>
+        <input type="text" id="username" placeholder="Ingresa tu nombre de usuario aquí" name="usuario" maxlength="15" required minlength="8" >
+        <div class="error">
+            <?php if(isset($error)) { echo $error; } ?>
+        </div>
+    </div>
+
+
             <div class="form-group">
                 <label for="email">Ingresa tu correo:</label><br>
-                <input type="email" id="email" placeholder="ejemplo@mail.com" name="email" required>
+                <input type="email" id="email" placeholder="ejemplo@mail.com" name="email"  required>
             </div>
 
             <div class="form-group">
                 <label for="telefono">Teléfono:</label><br>
-                <input type="text" id="telefono" placeholder="Ingresa tu telefono" name="telefono" required>
+                <input type="text" id="telefono" placeholder="Ingresa tu telefono" name="telefono" maxlength="15" required >
             </div>
 
             <div class="form-group">
                 <label for="password">Ingresa tu contraseña:</label><br>
-                <input type="password" id="password" placeholder="Ingresa tu contraseña aquí" name="password" required>
+                <input type="password" id="password" placeholder="Ingresa tu contraseña aquí" name="password" required maxlength="15" required minlength="8">
             </div>
             <div class="form-group">
                 <label for="confirm-password">Confirmar contraseña:</label><br>
-                <input type="password" id="confirm-password" placeholder="Ingresa tu contraseña aquí" name="pass" required>
+                <input type="password" id="confirm-password" placeholder="Ingresa tu contraseña aquí" name="pass" required maxlength="15" required minlength="8">
             </div>
 
             <button type="submit" class="btn btn-green" name="btncrearclient">Crear cuenta</button>
