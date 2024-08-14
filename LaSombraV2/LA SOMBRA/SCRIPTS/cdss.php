c6fb25d97dd3f98de6b636832fe83e23b9bf871f
jtalaveraa <jiramtalavera@hotmail.com>
2024-08-14 04:34:27 -0600

    ya se ven las imagenes y prodcutos en el inicio y meti chingos de imagenes, mañana paso la BD con todo

--- LaSombraV2/LA SOMBRA/SCRIPTS/cdss.php
+++ LaSombraV2/LA SOMBRA/SCRIPTS/cdss.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if ($_SESSION['rol'] == 3 || $_SESSION['rol'] == null) {
+if ($_SESSION['rol'] == 3 || $_SESSION['rol'] == 1 || $_SESSION['rol'] == 2 || $_SESSION['rol'] == 4 || $_SESSION['rol'] == null) {
     if (isset($_GET['marca'])) {    
         $_SESSION['marca'] = $_GET['marca'];    
     }
