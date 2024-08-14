0f2b350bf1e4dc433cde8699ffa4192c070745f4
Gera <go47455@gmail.com>
2024-08-14 12:35:34 -0600

    cambié la páginación

--- LaSombraV2/LA SOMBRA/VIEWS/productos.php
+++ LaSombraV2/LA SOMBRA/VIEWS/productos.php
@@ -290,15 +290,25 @@
         ?>           
             </div>
 
-            <div class="paginacion">
-            <?php
-            $pagina_actual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
-            for ($i = 1; $i <= $total_paginas; $i++) {
-                $active_class = $i == $pagina_actual ? 'active' : '';
-                echo "<a href='?pagina=" . $i . "' class='" . $active_class . "'>" . $i . "</a> ";
-            }
-            ?>
-            </div>
+            <nav aria-label="Paginación de productos">
+                    <ul class="pagination justify-content-center">
+                        <?php if ($pagina > 1) { ?>
+                            <li class="page-item">
+                                <a class="page-link" href="?pagina=<?= $pagina - 1 ?>">Anterior</a>
+                            </li>
+                        <?php } ?>
+                        <?php for ($i = max(1, $pagina - 2); $i <= min($total_paginas, $pagina + 2); $i++) { ?>
+                            <li class="page-item <?= $i == $pagina ? 'active' : '' ?>">
+                                <a class="page-link" href="?pagina=<?= $i ?>"><?= $i ?></a>
+                            </li>
+                        <?php } ?>
+                        <?php if ($pagina < $total_paginas) { ?>
+                            <li class="page-item">
+                                <a class="page-link" href="?pagina=<?= $pagina + 1 ?>">Siguiente</a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </nav>
         <?php  ?>
     </div>
         <footer class="footer row">
