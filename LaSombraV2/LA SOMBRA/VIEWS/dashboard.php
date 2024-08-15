aa0f8389dad36b777ac5b383a619301d9ba368af
aarale <vbaralezarate@gmail.com>
2024-08-14 21:46:45 -0600

    estoy tratando de responsabilizar

--- LaSombraV2/LA SOMBRA/VIEWS/dashboard.php
+++ LaSombraV2/LA SOMBRA/VIEWS/dashboard.php
@@ -110,198 +110,168 @@ include '../SCRIPTS/productos-dsh.php';
             <a href="../VIEWS/iniciov2.php">Ir a la pagina principal</a>
         </div>-->
         <div class="container-fluid">
-<div class="forms">
-    <form method="post" class="d-flex row" role="search">
-      <div class="col-lg-2 col-sm-6 p-1">
-      <input class="form-control" type="search" placeholder="ID Producto" aria-label="Search" id="id_prod" name="id_prod">
-      </div>
-      <div class="col-lg-3 col-sm-6 p-1">
-        <input class="form-control" type="search" placeholder="Nombre Producto" aria-label="Search" id="nm_prod" name="nm_prod">
-      </div>
-      <div class="col-lg-2 col-sm-6 p-1">
-        <select class="form-select" aria-label="Default select example" name="categoria">
-            <option selected value="">Categoria</option>
-            <option value="1">Pipas</option>
-            <option value="2">Bongs</option>
-            <option value="3">Canalas</option>
-            <option value="4">Hitters</option>
-            <option value="5">Electronicos</option>
-            <option value="6">Ropa</option>            
-            <option value="8">Blunts</option>            
-            <option value="7">Accesorios</option>
-        </select>
+    <div class="forms">
+        <form method="post" class="row g-3">
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <input class="form-control" type="search" placeholder="ID Producto" aria-label="Search" id="id_prod" name="id_prod">
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <input class="form-control" type="search" placeholder="Nombre Producto" aria-label="Search" id="nm_prod" name="nm_prod">
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <select class="form-select" aria-label="Default select example" name="categoria">
+                    <option selected value="">Categoria</option>
+                    <option value="1">Pipas</option>
+                    <option value="2">Bongs</option>
+                    <!-- Agrega el resto de opciones aquí -->
+                </select>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <select class="form-select" aria-label="Default select example" name="sucursal">
+                    <option selected value="">Sucursal</option>
+                    <option value="3">Todo</option>
+                    <option value="2">Nazas</option>
+                    <option value="1">Matamoros</option>
+                </select>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-12">
+                <button id="regprod" class="btn btn-primary w-100" type="submit" name="btn-aplicar">Aplicar</button>
+            </div>
+        </form>
+        
+        <?php if ($_SESSION['rol'] == 1) { ?>
+            <div class="row mt-3">
+                <div class="col-12">
+                    <button id="regprod" type='button' class='btn btn-success w-100' data-bs-toggle='modal' data-bs-target='#exampleModal'>Registrar Producto</button>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+    
+    <br>
+
+    <?php if ($results) { ?>
+        <h2>Resultados de búsqueda:</h2>
+        <div class="table-responsive">
+            <table class='table border='1' class='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Stock</th>
+                        <th>Precio</th>
+                        <th>Categoria</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($results as $row) { ?>
+                        <tr>
+                            <td><?= htmlspecialchars($row["id_producto"]); ?></td>
+                            <td><?= htmlspecialchars($row["nombre"]); ?></td>
+                            <td><?= htmlspecialchars($row["stock"]); ?></td>
+                            <td><?= htmlspecialchars($row["precio"]); ?></td>
+                            <td><?= htmlspecialchars($row["categoria"]); ?></td>
+                            <td>
+                                <a href='modificar.producto.php?id=<?= $row['id_producto']; ?>' class='btn btn-success'>
+                                    <i class='fa-solid fa-pen-to-square'></i>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
-        <div class="col-lg-2 col-sm-6 p-1">
-        <select class="form-select" aria-label="Default select example" name="sucursal">
-            <option selected value="">Sucursal</option>
-            <option value="3">Todo</option>
-            <option value="2">Nazas</option>
-            <option value="1">Matamoros</option>
-        </select>
-        </div>
-        <div class="col-lg-2 col-sm-6 p-1">
-      <button id="regprod" class="boton btn" type="submit" name="btn-aplicar">Aplicar</button>
-      </div>
-    </form>
-    <?php
-    if ($_SESSION['rol'] == 1) {  ?>         
-        <button id="regprod" type='button' class='btn col-lg-4 offset-4 p-1' data-bs-toggle='modal' 
-        data-bs-target='#exampleModal'> Registrar Producto </button>
+    <?php } else { ?>
+        <p>No se encontraron productos.</p>
     <?php } ?>
     
-    </div>
-<br>
-<?php
-    if ($results) {
-        echo "<h2>Resultados de búsqueda:</h2>";
-        
-        
-        echo "<div class='tabla'><table border='1' class= 'table table-striped'>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Stock</th>
-                    <th>Precio</th>
-                    <th>Categoria</th>
-                    <th><th>
-                </tr>";
-                foreach ($results as $row) {
-                  echo "<tr>";
-                  echo "<td>" . htmlspecialchars($row["id_producto"]) . "</td>";
-                  echo "<td>" . htmlspecialchars($row["nombre"]) . "</td>";
-                  echo "<td>" . htmlspecialchars($row["stock"]) . "</td>";
-                  echo "<td>" . htmlspecialchars($row["precio"]) . "</td>";
-                  echo "<td>" . htmlspecialchars($row["categoria"]) . "</td>";
-                  echo "<td>";
-                  echo "<a href='modificar.producto.php?id=" . $row['id_producto'] . "' class='btn btn-success'><i class='fa-solid fa-pen-to-square'></i></a>";
-                  echo "</td>";
-                  echo "</tr>";
-              }
+    <!-- Modal de Registro de Producto -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Registro de Producto</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="../SCRIPTS/productos-dsh.php" method="post" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label for="nombre">Nombre del Producto:</label>
+                                <input type="text" id="nombre" class="form-control" placeholder="Ingresa el nombre del producto" name="nombre">
+                            </div>
 
-        echo "</table>";
-    } else {
-        echo "<p>No se encontraron productos.</p>";
-    }
-?>
+                            <fieldset class="col-md-6">
+                                <legend>Marca:</legend>
+                                <div class="form-check">
+                                    <input type="radio" id="contactChoice1" name="contact" value="Raw" class="form-check-input" onclick="toggleInput()"/>
+                                    <label for="contactChoice1" class="form-check-label">Raw</label>
+                                </div>
+                                <!-- Agrega más opciones de marca aquí -->
+                                <div class="form-check">
+                                    <input type="radio" id="contactChoice7" name="contact" value="otro" class="form-check-input" onclick="toggleInput()"/>
+                                    <label for="contactChoice7" class="form-check-label">OTRO</label>
+                                </div>
+                            </fieldset>
 
-<form action="../SCRIPTS/productos-dsh.php" method="post" enctype="multipart/form-data">
-<div class="modal fade modal-lg" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Registro de Producto</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-            <form action="" method="post" enctype="multipart/form-data">
-                <div class="row">
-                    <div class="form-group">
-                        <label for="username">Nombre del Producto:</label><br>
-                        <input type="text" id="nombre" placeholder="Ingresa el nombre del producto" name="nombre">
-                    </div>                   
+                            <div id="otra_marca" class="col-md-6" style="display:none;">
+                                <label for="extra">Ingrese el Nombre de la Marca:</label>
+                                <input type="text" id="extra" class="form-control" name="extra">
+                            </div>
 
-                    <fieldset>
-                      <legend>MARCA:</legend>
-                      <div>
-                        <input type="radio" id="contactChoice1" name="contact" value="Raw" onclick="toggleInput()"/>
-                        <label for="contactChoice1">Raw</label>
+                            <fieldset class="col-md-6">
+                                <legend>Categoria(s)</legend>
+                                <?php foreach ($cat as $row) { ?>
+                                    <div class="form-check">
+                                        <input type="checkbox" id="coding" name="cate[]" value="<?= $row['id']; ?>" class="form-check-input" />
+                                        <label for="coding" class="form-check-label"><?= $row['nombre']; ?></label>
+                                    </div>
+                                <?php } ?>
+                            </fieldset>
 
-                        <input type="radio" id="contactChoice2" name="contact" value="Blazy Susan" onclick="toggleInput()"/>
-                        <label for="contactChoice2">Blazy Susan</label>
+                            <fieldset class="col-md-6">
+                                <legend>Proveedor(es)</legend>
+                                <p>(EL PRECIO DE COMPRA SE REGISTRA, DESDE EL REABASTECIMIENTO)</p>
+                                <?php foreach ($prov as $row) { ?>
+                                    <div class="form-check">
+                                        <input type="checkbox" id="<?= $row['id']; ?>" name="proveedores[]" value="<?= $row['id'];?>" class="form-check-input" />
+                                        <label for="<?= $row['id']; ?>" class="form-check-label"><?= $row['nombre']; ?></label>
+                                    </div>
+                                <?php } ?>
+                            </fieldset>
 
-                        <input type="radio" id="contactChoice3" name="contact" value="Rolling Circus" onclick="toggleInput()"/>
-                        <label for="contactChoice3">Rolling Circus</label>
+                            <div class="form-group col-md-6">
+                                <label for="precio">Precio al publico:</label>
+                                <input type="text" id="precio" class="form-control" placeholder="Ingresa el precio" name="precio">
+                            </div>
 
-                        <input type="radio" id="contactChoice3" name="contact" value="OCB" onclick="toggleInput()"/>
-                        <label for="contactChoice3">OCB</label>
+                            <fieldset class="col-md-6">
+                                <legend>Material:</legend>
+                                <!-- Agrega las opciones de materiales aquí -->
+                            </fieldset>
 
-                        <input type="radio" id="contactChoice3" name="contact" value="Kush" onclick="toggleInput()"/>
-                        <label for="contactChoice3">Kush</label>
+                            <div class="form-group col-md-12">
+                                <label for="img">Seleccione una imagen del producto:</label>
+                                <input type="file" id="img" class="form-control" name="img" onchange="validarTamanioArchivo(this)">
+                            </div>
 
-                        <input type="radio" id="contactChoice3" name="contact" value="Blunt Wrap" onclick="toggleInput()"/>
-                        <label for="contactChoice3">Blunt Wrap</label>
-
-                        <input type="radio" id="contactChoice3" name="contact" value="EYCE" onclick="toggleInput()"/>
-                        <label for="contactChoice3">EYCE</label>
-
-                        <input type="radio" id="contactChoice3" name="contact" value="otro" onclick="toggleInput()"/>
-                        <label for="contactChoice3">OTRO</label>
-                      </div>                
-                    </fieldset>
-
-                    <div id="otra_marca" style="display:none;">
-                      <label for="extra">Ingrese el Nombre de la Marca:</label>
-                      <input type="text" id="extra" name="extra">
-                    </div>
-                  
-                    <fieldset>
-                    <legend>Categoria(s)</legend>
-                    <?php 
-                    foreach ($cat as $row) { ?>
-                      <div>
-                        <input type="checkbox" id="coding" name="cate[]" value="<?php echo $row['id'];?>" />
-                        <label for="coding"><?php echo $row['nombre'];?></label>
-                      </div>
-                        <?php } ?>
-                    </fieldset>
-
-                    <fieldset>
-                    <legend>Proveedor(es)</legend>
-                    <p>(EL PRECIO DE COMPRA SE REGISTRA, DESDE EL REABASTECIMIENTO)</p>
-                    <?php 
-                    foreach ($prov as $row) { ?>
-                      <div>
-                        <input type="checkbox" id="<?php echo $row['id']; ?>" name="proveedores[]" value="<?php echo $row['id'];?>" />
-                        <label for="coding"><?php echo $row['nombre'];?></label>
-                      </div>
-                     <?php } ?>
-                    </fieldset>
-
-                    <div class="form-group">
-                        <label for="precio">Precio al publico:</label><br>
-                        <input type="text" id="nombre" placeholder="Ingresa el nombre del producto" name="precio">
-                    </div>
-
-                    <fieldset>
-                      <legend>MATERIAL:</legend>
-                      <div>
-                        <input type="radio" id="mt1" name="material" value="ceramica"/>
-                        <label for="contactChoice1">Ceramica</label>
-
-                        <input type="radio" id="mt22" name="material" value="metal"/>
-                        <label for="contactChoice2">Metal</label>
-
-                        <input type="radio" id="mt3" name="material" value="plastico"/>
-                        <label for="contactChoice3">Plastico</label>
-
-                        <input type="radio" id="mt4" name="material" value="cristal"/>
-                        <label for="contactChoice3">Cristal</label>
-
-                        <input type="radio" id="mt5" name="material" value="madera"/>
-                        <label for="contactChoice3">Madera</label> 
-                        
-                        <input type="radio" id="mt4" name="material" value="otro"/>
-                        <label for="contactChoice3">Otro</label>
-                      </div>                
-                    </fieldset>
-
-                    <label for="avatar">Seleccione una imagen del producto:</label>
-                    <input type="file" id="img" name="img"/>
-                      
-                    <legend>DESCRIPCIÓN:</legend>
-                     <textarea name="desc" class="form-control" rows="5"></textarea><br><br>
-                </div> 
-                <button id="regprod" type="submit" name="btnreg" class="btn">Registrar</button>     
-            </form>
+                            <div class="form-group col-md-12">
+                                <legend>Descripción:</legend>
+                                <textarea name="desc" class="form-control" rows="5"></textarea>
+                            </div>
+                        </div>
+                        <button type="submit" name="btnreg" class="btn btn-primary mt-3">Registrar</button>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
         </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>        
-      </div>
     </div>
-  </div>
 </div>
 
-</div>
 </div>
 </body>
 </html>
