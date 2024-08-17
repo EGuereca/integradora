e3bce2dbd77f53799a7745e8f0d703433cd68aab
jtalaveraa <jiramtalavera@hotmail.com>
2024-08-17 04:49:54 -0600

    unos estilillos

--- LaSombraV2/LA SOMBRA/VIEWS/clientes.php
+++ LaSombraV2/LA SOMBRA/VIEWS/clientes.php
@@ -1,11 +1,89 @@
+<?php
+session_start();
+if ($_SESSION["rol"] != 1|| $_SESSION["rol"] == null) {
+    header("location: ../VIEWS/iniciov2.php");
+    exit();    
+}
+$nombreUsuario = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : 'Usuario';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cliente</title>
+    <link rel="stylesheet" href="../bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <script src="../bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/49e84e2ffb.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../CSS/dash-clientes.css">
 </head>
 <body>
-    Hola
+    <div class="d-flex">
+    <header>
+    <nav id="contenedor-todo" class="navbar  fixed-top">
+    <div  class="container">
+    <div class="row align-items-center">
+    <div class="col-6 col-lg-4 order-2 order-lg-4">
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+            <div class="offcanvas-header">
+                <div class="logo">
+                <a href="#in"><img src="../IMG/sombra-logo.png" alt="La Sombra"></a>
+                </div>
+                <button type="button" class="btn-close btn-close-black" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div  id="body-burger"   class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../VIEWS/dash-ventas.php">VENTAS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../VIEWS/dash-apartados.php">PEDIDOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../VIEWS/dashboard.php">PRODUCTOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../VIEWS/dash-citas.php">CITAS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../VIEWS/dash-provee.php">PROVEEDOR</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../VIEWS/dsh-empl.php">EMPLEADOS</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="../VIEWS/reabastecimiento.php">REABASTECIMIENTO</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" style="background-color: limegreen;" href="#">CLIENTES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../VIEWS/iniciov2.php">IR A LA PÁGINA PRINCIPAL</a>
+                    </li>
+                    <div class="usuario-info">
+                    <p>Bienvenido, <?php echo htmlspecialchars($nombreUsuario); ?>!</p>
+                        <a href="?logout=1" class="logout-icon">
+                        <i class="fas fa-sign-out-alt"></i> 
+                        </a>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    </nav>
+    </header>
+
+    
+
+
+
+    </div>
 </body>
 </html>
